be16b8324e0b37b7b59290f26de1d4a183f454f3
kasettakorn <kasettakorn_techno@hotmail.com>
2020-02-02 20:26:44 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Layout, Menu, Icon } from 'antd';
+import SubMenu from 'antd/lib/menu/SubMenu';
+import 'antd/dist/antd.css';
+import Bisection from './pages/Bisection'
+const { Header, Content, Footer, Sider } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  rootSubmenuKeys = ['unit1', 'unit2', 'unit3'];
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      openKeys: [''],
+    }
+
+  }
+
+
+  render() {
+    return (
+      <Router>
+        <Layout>
+          <Sider width={250} style={{
+            overflow: 'auto',
+            height: '100vh',
+
+            position: 'fixed',
+
+          }}
+          >
+            <div className="logo" />
+            <Menu theme="dark" mode="inline">
+              <SubMenu key="unit1"
+                title={
+                  <span>
+                    <Icon type="calculator" />Root of equation
+              </span>
+                }>
+                <Menu.Item key="graphical_btn">Graphical</Menu.Item>
+                <Menu.Item key="bisection_btn">Bisection<Link to="/bisection" /></Menu.Item>
+                <Menu.Item key="false_btn">False Position</Menu.Item>
+                <Menu.Item key="onepoint_btn">One Point Iteration</Menu.Item>
+                <Menu.Item key="newton_btn">Newton Raphson</Menu.Item>
+                <Menu.Item key="secant_btn">Secant Method</Menu.Item>
+
+              </SubMenu>
+
+            </Menu>
+            <Menu theme="dark" mode="inline">
+              <SubMenu key="unit2" title={
+                <span>
+                  <Icon type="user" />Linear Algebra
+              </span>
+              }>
+                <Menu.Item key="graphical_btn">Cramer's Rule</Menu.Item>
+                <Menu.Item key="bisection_btn">Gauss's Elimination</Menu.Item>
+                <Menu.Item key="false_btn">Gauss Jordan Method</Menu.Item>
+                <Menu.Item key="onepoint_btn">Matrix Inversion</Menu.Item>
+                <Menu.Item key="newton_btn">LU Decomposition</Menu.Item>
+                <Menu.Item key="secant_btn">Cholesky Decomposition</Menu.Item>
+                <Menu.Item key="secant_btn">Jacobi Iteration Method</Menu.Item>
+                <Menu.Item key="secant_btn">Gauss Seidel Iteration Method</Menu.Item>
+                <Menu.Item key="secant_btn">Conjugate Gradient Method</Menu.Item>
+
+              </SubMenu>
+
+            </Menu>
+            <Menu theme="dark" mode="inline">
+              <SubMenu key="unit3" title={
+                <span>
+                  <Icon type="stock" />Interpolation
+              </span>
+              }>
+                <Menu.Item key="graphical_btn">Newton Divided-Difference</Menu.Item>
+                <Menu.Item key="bisection_btn">Lagrange</Menu.Item>
+                <Menu.Item key="false_btn">Spline</Menu.Item>
+
+              </SubMenu>
+
+            </Menu>
+          </Sider>
+          <Layout style={{ marginLeft: 250 }}>
+            <Content style={{ margin: '24px 16px 0', overflow: 'initial', height: '100%' }}>
+              <Route path="/bisection" component={Bisection} />
+            </Content>
+
+          </Layout>
+        </Layout>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/pages/Bisection.js
@@ -0,0 +1,202 @@
+import React, { Component } from 'react'
+import { Card, Input, Button, Table, Col, Row } from 'antd';
+import 'antd/dist/antd.css';
+import { range, compile } from 'mathjs'
+import Plot from 'react-plotly.js';
+const InputStyle = {
+
+    color: "black",
+    fontWeight: "bold",
+    fontSize: "24px"
+
+};
+var dataInTable = []
+const columns = [
+    {
+        title: "Iteration",
+        dataIndex: "iteration",
+        key: "iteration"
+    },
+    {
+        title: "XL",
+        dataIndex: "xl",
+        key: "xl"
+    },
+    {
+        title: "XR",
+        dataIndex: "xr",
+        key: "xr"
+    },
+    {
+        title: "X",
+        dataIndex: "x",
+        key: "x"
+    },
+    {
+        title: "Error",
+        key: "error",
+        dataIndex: "error"
+    }
+];
+const xValues = range(-10, 10, 0.5).toArray();
+var fx = " ";
+class Bisection extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            fx: "",
+            xl: 0,
+            xr: 0,
+            showOutputCard: false,
+            showGraph: false,
+            moveLeft: false
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.bisection = this.bisection.bind(this);
+    }
+    bisection(xl, xr) {
+        fx = this.state.fx;
+        var increaseFunction = false;
+        var xm = 0;
+        var sum = parseFloat(0.000000);
+        var n = 0;
+        var data = []
+        data['xl'] = []
+        data['xr'] = []
+        data['x'] = []
+        data['error'] = []
+        if (this.func(xl) < this.func(xr)) {
+            increaseFunction = true;
+        }
+
+        do {
+            xm = (xl + xr) / 2;
+            if (this.func(xm) * this.func(xr) < 0) {
+                sum = this.error(xm, xr);
+                if (increaseFunction) {
+                    xl = xm;
+                }
+                else {
+                    xr = xm;
+                }
+
+            }
+            else {
+                sum = this.error(xm, xl);
+                if (increaseFunction) {
+                    xr = xm;
+                }
+                else {
+                    xl = xm;
+                }
+            }
+            data['xl'][n] = xl;
+            data['xr'][n] = xr;
+            data['x'][n] = xm.toFixed(8);
+            data['error'][n] = Math.abs(sum).toFixed(8);
+            n++;
+        } while (Math.abs(sum) > 0.000001);
+        this.createTable(data['xl'], data['xr'], data['x'], data['error']);
+        this.setState({
+            showOutputCard: true,
+            showGraph: true
+        })
+
+
+    }
+    func(X) {
+        var expr = compile(this.state.fx);
+        let scope = { x: parseFloat(X) };
+        return expr.eval(scope);
+    }
+    error(xnew, xold) {
+        return Math.abs((xnew - xold) / xnew);
+    }
+    createTable(xl, xr, x, error) {
+        dataInTable = []
+        for (var i = 0; i < xl.length; i++) {
+            dataInTable.push({
+                iteration: i + 1,
+                xl: xl[i],
+                xr: xr[i],
+                x: x[i],
+                error: error[i]
+            });
+        }
+
+    }
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+
+    }
+    render() {
+        return (
+            <div style={{ background: "#FFFF", padding: "30px" }}>
+                <h2 style={{ color: "black", fontWeight: "bold" }}>Bisection Method</h2>
+                <Row>
+                    <Col span={12}>
+                        <Card
+                            title={"Input Bisection"}
+                            bordered={true}
+                            style={{ background: "gray" }}
+                            onChange={this.handleChange}
+                            id="inputCard"
+                        >
+                            <h2>f(x)</h2><Input size="large" name="fx" style={InputStyle}></Input>
+                            <h2>X<sub>L</sub></h2><Input size="large" name="xl" style={InputStyle}></Input>
+                            <h2>X<sub>R</sub></h2><Input size="large" name="xr" style={InputStyle}></Input><br /><br />
+                            <Button id="submit_button" onClick={
+                                () => this.bisection(parseFloat(this.state.xl), parseFloat(this.state.xr))
+                            }
+                                style={{ background: "#4caf50", color: "white", fontSize: "20px" }}>Submit</Button>
+
+                        </Card>
+                    </Col>
+                    <Col span={12}>
+                        {this.state.showGraph &&
+                                <Plot
+                                    
+                                    data={[
+                                        {
+                                            x: range(-10, 10, 0.5).toArray(),
+                                            y: xValues.map(function (x) {
+                                                return compile(fx).eval({ x: x })
+                                            }),
+                                            type: 'scatter',
+                                            marker: { color: 'red' },
+                                        },
+                                    ]}
+                                    layout={{ title: 'Bisection' }}
+
+                                    style={{ width: "100%" }}
+                                />
+                         
+                        }
+                    </Col>
+
+                </Row>
+
+
+
+                {this.state.showOutputCard &&
+                    <Card
+                        title={"Output"}
+                        bordered={true}
+                        style={{ width: "100%", background: "#2196f3", color: "#FFFFFFFF", float: "inline-start", marginBlockStart: "2%" }}
+                        id="outputCard"
+                    >
+                        <Table columns={columns} dataSource={dataInTable} bodyStyle={{ fontWeight: "bold", fontSize: "18px", color: "black" }}
+                        ></Table>
+                    </Card>
+                }
+
+
+
+            </div>
+        );
+    }
+}
+export default Bisection;
